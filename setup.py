79740c2dab119476119d36cdd32bc5c7b21c1188
Mitar <mitar.git@tnode.com>
2013-03-09 22:38:17 -0800

    Tabs to spaces.

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ VERSION = '0.1'
 PACKAGE = 'sqlauthstore'
 
 setup(
-	name = 'SQLAuthStorePlugin',
-	version = VERSION,
-	description = "SQL password store for Trac's AccountManager.",
-	author = 'Mitar',
-	author_email = 'mitar.trac@tnode.com',
-	url = 'http://mitar.tnode.com/',
-	keywords = 'trac plugin',
-	license = 'GPLv3',
-	packages = [PACKAGE],
-	include_package_data = True,
-	install_requires = [
-		'TracAccountManager',
-	],
-	zip_safe = False,
-	entry_points = {
-		'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE),
-	},
+    name = 'SQLAuthStorePlugin',
+    version = VERSION,
+    description = "SQL password store for Trac's AccountManager.",
+    author = 'Mitar',
+    author_email = 'mitar.trac@tnode.com',
+    url = 'http://mitar.tnode.com/',
+    keywords = 'trac plugin',
+    license = 'GPLv3',
+    packages = [PACKAGE],
+    include_package_data = True,
+    install_requires = [
+        'TracAccountManager',
+    ],
+    zip_safe = False,
+    entry_points = {
+        'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE),
+    },
 )
